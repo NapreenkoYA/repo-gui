ee192ac68ccfa613f26b3839886cfcfad938547c
NapreenkoYA <99269745+NapreenkoYA@users.noreply.github.com>
2022-12-09 15:50:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,24 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/*
-1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
-2. Написать метод, который преобразует массив в ArrayList;
-3. Большая задача:
-a. Есть классы Fruit -> Apple, Orange;(больше фруктов не надо)
-b. Класс Box в который можно складывать фрукты, коробки условно сортируются по типу фрукта, поэтому в одну коробку
- нельзя сложить и яблоки, и апельсины;
-c. Для хранения фруктов внутри коробки можете использовать ArrayList;
-d. Сделать метод getWeight() который высчитывает вес коробки, зная количество фруктов и вес одного фрукта(вес яблока
- - 1.0f, апельсина - 1.5f, не важно в каких это единицах);
-e. Внутри класса коробка сделать метод compare, который позволяет сравнить текущую коробку с той, которую подадут
- в compare в качестве параметра, true - если их веса равны, false в противном случае(коробки с яблоками мы можем
-  сравнивать с коробками с апельсинами);
-f. Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую коробку(помним про сортировку
- фруктов, нельзя яблоки высыпать в коробку с апельсинами), соответственно в текущей коробке фруктов не остается,
-  а в другую перекидываются объекты, которые были в этой коробке;
-g. Не забываем про метод добавления фрукта в коробку.
- */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -35,12 +18,7 @@ public class Main {
         String[] arrayOfStrings = {"A", "B", "C", "D"};
         asList(arrayOfStrings);
         //Task3
-        //извиняюсь за такое большое кол-во кода при тестинге задачи 3. Возможно стоило написать
-        //метод для теста. Немного не успела
-//        Box<Orange> or = new Box<>(new Orange(),new Orange(),new Orange(),new Orange());
-//        Box<Orange> or1 = new Box<>(new Orange(),new Orange());
-//        Box<Apple> ap = new Box<>(new Apple(),new Apple(),new Apple(),new Apple());
-//        Box<Apple> ap1 = new Box<>(new Apple(),new Apple(),new Apple());
+        
         Box<Orange> or = new Box<>();
         Box<Orange> or1 = new Box<>();
         Box<Apple> ap = new Box<>();
@@ -74,15 +52,11 @@ public class Main {
         Object sw = arr[n1];
         arr[n1]=arr[n2];
         arr[n2]=sw;
-        System.out.println("The result of the replacement: "+Arrays.toString(arr)+"\n================================");
+        System.out.println("The result of the replacement: "+Arrays.toString(arr)+"\n");
     }
     public static <T> void asList(T[]arr){
-//        почему нельзя использовать метод addAll с T?
-//        List<String> listOfStrings = new ArrayList<>();
-//        Collections.addAll(listOfStrings,arr);
-//        for (String str : listOfStrings)
-//            System.out.print(" " + str);
+
         ArrayList<T> alt = new ArrayList<>(Arrays.asList(arr));
-        System.out.println("Task2 and the result of the conversion : "+alt+"\n================================");
+        System.out.println("Task2 and the result of the conversion : "+alt+"\n");
     }
 }
