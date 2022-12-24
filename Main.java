2438cce8418f951c1dd5b7389ff71d352eda385f
NapreenkoYA <99269745+NapreenkoYA@users.noreply.github.com>
2022-12-24 11:34:41 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,70 @@
-package lesson8;
+package lesson9;
 
-import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Создаем экземпляр по работе с БД
-            DbHandler dbHandler = DbHandler.getInstance();
-            // Добавляем запись
-            //dbHandler.addProduct(new Product("Музей", 200, "Развлечения"));
-            // Получаем все записи и выводим их на консоль
-            List<Product> products = dbHandler.getAllProducts();
-            for (Product product : products) {
-                System.out.println(product.toString());
-            }
-            // Удаление записи с id = 8
-            //dbHandler.deleteProduct(8);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Course course1 = new CourseImpl("Java");
+        Course course2 = new CourseImpl("JavaCore");
+        Course course3 = new CourseImpl("Linux");
+
+        List<Student> students = Arrays.asList(
+                new StudentImpl("Ivanov", Arrays.asList(course1, course2)),
+                new StudentImpl("Petrov", Arrays.asList(course2, course3)),
+                new StudentImpl("Andreev", Arrays.asList(course1, course3)),
+                new StudentImpl("Sidorov", Arrays.asList(course1, course2, course3)),
+                new StudentImpl("Antonov", null)
+        );
+
+        System.out.println(getUniqueCourses(students));
+        System.out.println(getInquisitiveStudent(students));
+        System.out.println(getStudentsByCourses(students, course2));
+    }
+
+    public static List<Course> getUniqueCourses(List<Student> students) {
+        students = students == null ? new ArrayList<>() : students;
+
+        return students.stream()
+                .filter(Objects::nonNull)
+                .map(Student::getAllCourses)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Student> getInquisitiveStudent(List<Student> students) {
+        students = students == null ? new ArrayList<>() : students;
+
+        return students.stream()
+                .filter(Objects::nonNull)
+                .sorted((o1, o2) -> {
+                    List<Course> c1 = o1.getAllCourses();
+                    List<Course> c2 = o2.getAllCourses();
+                    return Integer.compare(
+                            c2 == null ? 0 : c2.size(),
+                            c1 == null ? 0 : c1.size()
+                    );
+                })
+                .limit(3)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Student> getStudentsByCourses(List<Student> students, Course course) {
+        if (course == null) {
+            return new ArrayList<>();
         }
+
+        students = students == null ? new ArrayList<>() : students;
+
+        return students.stream()
+                .filter(Objects::nonNull)
+                .filter(student -> {
+                    List<Course> courses = student.getAllCourses();
+                    courses = courses == null ? Collections.emptyList() : courses;
+                    return courses.contains(course);
+                })
+                .collect(Collectors.toList());
     }
 }
