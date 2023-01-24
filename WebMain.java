9b00f4622d21fbf8fd7ba10f8146126432d06018
NapreenkoYA <99269745+NapreenkoYA@users.noreply.github.com>
2023-01-24 19:22:56 +0300

    Add files via upload

---
+++ WebMain.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+public class ExampleMain {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+        @@ -63,6 +65,7 @@ public static void main(String[] args) {
+            System.out.println(e.getSupportUrl());
+        }
+
+    }
+}
