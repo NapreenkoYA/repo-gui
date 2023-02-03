68b106d2cc41210015b954d45a6be16ee375a556
NapreenkoYA <99269745+NapreenkoYA@users.noreply.github.com>
2023-02-03 20:06:40 +0300

    Update WebMain.java

--- WebMain.java
+++ WebMain.java
@@ -1,28 +1,63 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import javax.sound.midi.Soundbank;
-import java.util.ArrayList;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-public class WebMain {
-    public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("start-maximized");
+public class MainMenu extends AbstractPage {
 
+    @FindBy(xpath = ".//ul[@class='menu']/li//a[@href='/abit/']")
+    private WebElement abit;
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://google.com");
-        @@ -63,6 +65,7 @@ public static void main(String[] args) {
-            System.out.println(e.getSupportUrl());
-        }
+    @FindBy(xpath = ".//ul[@class='menu']/li//a[@href='/abit/']/../ul/li")
+    private List<WebElement> subAbitList;
 
+    @FindBy(xpath = ".//ul[@class='menu']/li//a[@href='/student/']")
+    private WebElement student;
+
+    @FindBy(xpath = ".//ul[@class='menu']/li//a[@href='/nauka/otdel-aspirantury-i-doktorantury/']")
+    private WebElement nauka;
+
+    @FindBy(xpath = ".//ul[@class='menu']/li//a[@href='/sotrudniku/']")
+    private WebElement sotrudniku;
+
+    @FindBy(xpath = ".//ul[@class='menu']/li//a[@href='/vypuskniku/']")
+    private WebElement vypuskniku;
+
+    public MainMenu(WebDriver driver) {
+        super(driver);
     }
+
+    public MainMenu clickabit(){
+        abit.click();
+        return this;
+    }
+
+    public MainMenu clickstudent(){
+        student.click();
+        return this;
+    }
+
+    public MainMenu clicknauka(){
+        nauka.click();
+        return this;
+    }
+
+    public MainMenu clicksotrudniku(){
+        sotrudniku.click();
+        return this;
+    }
+
+    public MainMenu clickvypuskniku(){
+        vypuskniku.click();
+        return this;
+    }
+
+    public void clickWelcome(){
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(abit).click(subAbitList.get(0)).build().perform();
+    }
+
+
 }
